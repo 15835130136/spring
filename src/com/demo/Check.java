64767d01a3d7fe49b1f9753adf5982958faf9bd3
hjdeng <hjdeng@amarsoft.com>
2017-10-18 21:50:25 +0800

    1

---
+++ src/com/demo/Check.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Check {
+	
+	public void beforeTest() {
+		System.out.println("person要开始用斧头了！");
+	}
+	
+	public void afterTest() {
+		System.out.println("person使用完斧头了!");
+	}
+
+}
